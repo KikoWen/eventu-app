1003d3589e5810d6c3dd2509186d195a4fc486c6
Juhee Han <200859841@cit3-501m-05.local>
2019-09-12 13:23:49 +1200

    we did some more :)

--- src/RouteAddEvent.jsx
+++ src/RouteAddEvent.jsx
@@ -9,13 +9,15 @@ class RouteAddEvent extends Component{
     var formData = new FormData(this.form);
 
     var data = {
-      name:formData.get('name-input'),
-      description:formData.get('description-input'),
-      location:formData.get('location-input'),
-      time:formData.get('time-input'),
-      category:formData.get('category-input'),
+      name:formData.get('eventName'),
+      category:formData.get('eventType'),
+      location:formData.get('eventLocation'),
+      description:formData.get('eventDescription'),
+      price:formData.get('ticketPrice'),
     }
-    addEvents(data).then(res => navigate('/events'))
+
+    console.log(data);
+    addEvents(data).then(res => navigate('/'))
   }
     
  
@@ -32,12 +34,12 @@ class RouteAddEvent extends Component{
             <form onSubmit={this.handleFormSubmit} ref={(el) => {this.form = el}}>
                 <div class="form-group">
                     <label for="eventName">Event Name</label>
-                    <input type="name" class="form-control" id="eventName"/>
+                    <input type="name" class="form-control" name="eventName" id="eventName"/>
                 </div>
                 <div class="event-type-and-location">
                     <div class="form-group">
                         <label for="eventType">Event Type</label>
-                        <select class="form-control" id="">
+                        <select class="form-control" id="eventType" name="eventType">
                             <option>Entertainment</option>
                             <option>Wellbeing</option>
                             <option>Sport</option>
@@ -47,23 +49,24 @@ class RouteAddEvent extends Component{
                     </div>
                     <div className="form-group">
                         <label for="eventLocation">Event Location</label>
-                        <input type="location" class="form-control" id="eventLocation"/>
+                        <input type="location" class="form-control" name="eventLocation" id="eventLocation"/>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label for="exampleFormControlTextarea1">Event Description</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="4"></textarea>
+                    <label for="eventDescription">Event Description</label>
+                    <textarea class="form-control" name="eventDescription" id="eventDescription" rows="4"></textarea>
                 </div>
                 <div class="form-group">
                     <label for="uploadPhoto">Upload Photo</label>
-                    <input type="file" class="form-control-file" id="uploadPhoto"/>
+                    <input type="file" class="form-control-file" name="uploadPhoto" id="uploadPhoto"/>
                 </div>
                 <div class="form-group">
                     <label for="ticketPrice">Event Name</label>
-                    <input type="price" class="form-control" id="ticketPrice"/>
+                    <input type="price" class="form-control" name="ticketPrice" id="ticketPrice"/>
                 </div>
+                <button type="submit" className="btn btn-primary">PLEEaaESSSsse</button>
             </form>
-            <button type = "submit" className="btn btn-primary">PLEEaaESSSsse</button>
+            
         </div>
         <div class="footer">
             <div class="home-footer">
